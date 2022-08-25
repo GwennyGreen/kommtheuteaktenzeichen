57f660b9829642ae59651fe159eb5c52f99dc813
Claudia Pellegrino <claui@users.noreply.github.com>
2022-08-25 13:23:12 +0200

    Honor argv even if args are given

--- kha/cli.py
+++ kha/cli.py
@@ -9,9 +9,6 @@ from . import api, fire_workarounds
 
 def run(*args: str) -> None:
     """Runs the command line interface."""
-    if not args:
-        args = tuple(sys.argv[1:])
-
     fire_workarounds.apply()
     fire.Fire({
         'check': api.check_episode,
@@ -20,5 +17,5 @@ def run(*args: str) -> None:
             'dev': api.print_episodes_dev,
             'prod': api.print_episodes_prod,
         }
-    }, command=args
+    }, command=list(args) + sys.argv[1:]
     )
